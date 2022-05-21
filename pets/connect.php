ec199a54fc2ac2bfd81207e401446e31557512d0
Marahalkhateeb12 <marahalkhateeb12@gmail.com>
2022-05-21 16:09:47 +0300

    main

---
+++ pets/connect.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databaseName = "ecommerce";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$databaseName", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+
+    // echo error message 
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
